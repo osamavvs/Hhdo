1aa0de74376c0cd829a26e48269e21e160f1edd5
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 08:17:09 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ class Config:
     API_HASH = "5b8a468627791bdd36a8c361913b0b72"
     BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"
     OWNER_ID = 8074717568
+    CH_USER = "BBABB9" # يوزر القناة بدون @
