12f814deec2a28b052c3f489c872db3916e777ce
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 06:47:07 +0300

    Update protect.py

--- plugins/protect.py
+++ plugins/protect.py
@@ -1,1 +1,15 @@
+from pyrogram import Client, filters
+from config import Config
+import asyncio
 
+@Client.on_message((filters.regex(r"https?://[^\s]+") | filters.forwarded) & filters.group)
+async def group_protector(client, message):
+    if message.from_user.id == Config.OWNER_ID:
+        return
+    try:
+        await message.delete()
+        warn = await message.reply_text(f"⚠️ {message.from_user.mention} ممنوع الروابط والتوجيه هنا!")
+        await asyncio.sleep(3)
+        await warn.delete()
+    except:
+        pass
