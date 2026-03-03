9d1694ba5d28f7784272296257d63f8665c17733
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 09:08:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,18 @@
 class Config:
-    # --- بيانات الربط الأساسية ---
+    # --- بيانات الربط والتوكن ---
     API_ID = 24752047
     API_HASH = "5b8a468627791bdd36a8c361913b0b72"
+    BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"
     
     # --- قاعدة البيانات ---
     MONGO_DB_URI = "mongodb+srv://asopes111_db_user:urKzCqP4yrMpSUyq@cluster0.haza5xq.mongodb.net/?appName=Cluster0"
     
     # --- الهوية والإشعارات ---
     OWNER_ID = 8074717568
-    LOG_GROUP_ID = -1005129765852  # أضفت لك -100 لأن التليجرام يحتاجها للكروبات
+    LOG_GROUP_ID = -1005129765852
     
-    # --- حقوق السورس ---
+    # --- حقوق السورس (تم التعديل) ---
     OWNER_USERNAME = "U_K44"
     CH_USER = "BBABB9"
+    BOT_NAME = "كرستال"  # تم إلغاء يوكي ووضع اسم سورس كرستال
+    MUSIC_BOT_NAME = "كرستال"
