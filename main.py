4756775fe37bb4e493591afa0047adcc8cf60280
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 12:58:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from pyrogram import Client
 from config import Config
+import os
 
-# إعداد البوت
+# إنشاء كائن البوت
 app = Client(
     "CrystalBot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="plugins") # هذا السطر يخليك تعدل أي ملف في مجلد plugins ويشتغل تلقائياً
+    plugins=dict(root="plugins")
 )
 
 if __name__ == "__main__":
-    print("💎 سورس كريستال يعمل الآن.. بانتظار تعديلاتك يا أسامة!")
+    print("🚀 جاري فحص الملفات...")
+    if not os.path.exists("plugins"):
+        os.makedirs("plugins")
+    print(f"✅ تم العثور على المجلدات. جاري تشغيل البوت...")
     app.run()
