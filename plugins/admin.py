54caa0de0b2750c64f462204a845a70603670644
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 22:09:25 +0300

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,15 +1,5 @@
-cat << 'EOF' > ~/Hhdo/plugins/admin.py
 from pyrogram import Client, filters
 
-@Client.on_message(filters.command("الاوامر", "") | filters.regex("^الاوامر$"))
-async def commands(client, message):
-    await message.reply_text("💎 **أهلاً بك يا عمدة في قائمة الأوامر:**\n\n• `ايدي` : لعرض آيديك\n• `بوت` : للتأكد من التشغيل")
-
-@Client.on_message(filters.command("ايدي", "") | filters.regex("^ايدي$"))
-async def my_id(client, message):
-    await message.reply_text(f"✨ **آيديك هو:** `{message.from_user.id}`")
-
-@Client.on_message(filters.regex("^بوت$"))
-async def bot_test(client, message):
-    await message.reply_text("✅ **لبيـه! البوت شغال وعال العال.**")
-EOF
+@Client.on_message(filters.command("id") & filters.private)
+async def get_id(bot, message):
+    await message.reply_text(f"ايديك هو: <code>{message.from_user.id}</code>", parse_mode="html")
