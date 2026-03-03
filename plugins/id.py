f6c87dc451800cbb1569284bad6131bc4bcbcabc
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 11:47:42 +0300

    Delete plugins/id.py

--- plugins/id.py
+++
@@ -1,9 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.regex("^(ايدي|id|ID)$"))
-async def get_my_id(client, message):
-    await message.reply_text(f"👤 اسمك: {message.from_user.first_name}\n🆔 ايديك: `{message.from_user.id}`")
-
-@Client.on_message(filters.regex("^(اوامر|الاوامر)$"))
-async def help_list(client, message):
-    await message.reply_text("💎 **أوامر كرستال المنظمة:**\n1- ايدي\n2- مرحبا\n3- سورس")
