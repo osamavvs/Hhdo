b8c5086d30f1814d9b02ac1405e3be1f11ee7759
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 06:46:53 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,1 +1,9 @@
+from pyrogram import Client, filters
 
+@Client.on_message(filters.command("start") & filters.private)
+async def start_cmd(client, message):
+    await message.reply_text(f"💎 أهلاً بك في **سورس كرستال** المنظم!\nاستخدم كلمة 'اوامر' لرؤية ما يمكنني فعله.")
+
+@Client.on_message(filters.regex("^(مرحبا|هلا|هلو)$") & filters.private)
+async def hello_text(client, message):
+    await message.reply_text("يا هلا بك في عالم الكرستال! ✨")
