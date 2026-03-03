fb98de3dc2e3d44f0722d5a51b307ee10386a571
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 09:12:05 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 class Config:
-    # --- بيانات الربط والتوكن ---
+    # --- بيانات الربط (الجديدة والشغالة) ---
     API_ID = 24752047
     API_HASH = "5b8a468627791bdd36a8c361913b0b72"
     BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"
@@ -7,12 +7,12 @@ class Config:
     # --- قاعدة البيانات ---
     MONGO_DB_URI = "mongodb+srv://asopes111_db_user:urKzCqP4yrMpSUyq@cluster0.haza5xq.mongodb.net/?appName=Cluster0"
     
-    # --- الهوية والإشعارات ---
+    # --- هويتك (أسامة) ---
     OWNER_ID = 8074717568
-    LOG_GROUP_ID = -1005129765852
+    SUDO_USERS = [8074717568]
     
-    # --- حقوق السورس (تم التعديل) ---
+    # --- الإشعارات والحقوق ---
+    LOG_GROUP_ID = -1005129765852
     OWNER_USERNAME = "U_K44"
     CH_USER = "BBABB9"
-    BOT_NAME = "كرستال"  # تم إلغاء يوكي ووضع اسم سورس كرستال
-    MUSIC_BOT_NAME = "كرستال"
+    BOT_NAME = "سورس كرستال"
