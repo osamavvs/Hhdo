5d21ee9d141f6e9b6ab779ffd4026f3003d65955
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 22:14:14 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-# البيانات سيتم ملؤها تلقائياً بواسطة السيرفر
+# بياناتك ثابتة وجاهزة
 API_ID = 24752047
 API_HASH = "5b8a468627791bdd36a8c361913b0b72"
 BOT_TOKEN = "8721155986:AAHdipR_Xg6YUebhq_FWU3_oeHjyNdePT_c"
@@ -12,12 +12,12 @@ class Bot(Client):
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
-            plugins=dict(root="plugins") # هذا يخلي البوت يقرأ كل شي داخل مجلد plugins
+            plugins=dict(root="plugins") # هذا يقرأ كل الملفات داخل مجلد plugins تلقائياً
         )
 
     async def start(self):
         await super().start()
-        print("🚀 البوت اشتغل وبدأ يقرأ ملفات الأزرار من plugins...")
+        print("🚀 تم تشغيل البوت بنجاح.. أرسل /start في التليجرام")
 
     async def stop(self, *args):
         await super().stop()
