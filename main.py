ad99751dc510499b90b40cf2f4ebe0a650d66a04
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 12:51:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 from pyrogram import Client
 from config import Config
 
-# إعداد المحرك لقراءة المجلدات تلقائياً
-bot = Client(
-    "Crystal",
+# إعداد البوت
+app = Client(
+    "CrystalBot",
     api_id=Config.API_ID,
     api_hash=Config.API_HASH,
     bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="plugins") # هذا هو السطر اللي يخلي البوت يقرأ ملفات الحماية والترحيب
+    plugins=dict(root="plugins") # هذا السطر يخليك تعدل أي ملف في مجلد plugins ويشتغل تلقائياً
 )
 
-print("--- 💎 سورس كرستال (النظام المنظم) يعمل الآن بنجاح ---")
-bot.run()
+if __name__ == "__main__":
+    print("💎 سورس كريستال يعمل الآن.. بانتظار تعديلاتك يا أسامة!")
+    app.run()
