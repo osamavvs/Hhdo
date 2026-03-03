cf0b8cdcfbb4ef36470144a39099b943ad7685a1
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 12:31:38 +0300

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,14 +1,25 @@
-from pyrogram import filters
-from pyrogram.types import ChatPermissions
-from main import app
+from telebot.types import ChatPermissions
 
-@app.on_message(filters.command("حظر") & filters.group)
-async def ban_user(client, message):
-    if message.reply_to_message:
-        user_id = message.reply_to_message.from_user.id
-        await client.ban_chat_member(message.chat.id, user_id)
-        await message.reply(
-"━━━━━━━━━━━━━━━━━━\n"
-"🚫 تم حظر العضو\n"
-"━━━━━━━━━━━━━━━━━━"
-)
+def تسجيل(bot):
+
+    @bot.message_handler(commands=['كتم'])
+    def كتم(message):
+        if message.reply_to_message:
+            user_id = message.reply_to_message.from_user.id
+            bot.restrict_chat_member(
+                message.chat.id,
+                user_id,
+                ChatPermissions(can_send_messages=False)
+            )
+            bot.reply_to(message, "تم كتم العضو 🔇")
+
+    @bot.message_handler(commands=['الغاء_كتم'])
+    def الغاء_كتم(message):
+        if message.reply_to_message:
+            user_id = message.reply_to_message.from_user.id
+            bot.restrict_chat_member(
+                message.chat.id,
+                user_id,
+                ChatPermissions(can_send_messages=True)
+            )
+            bot.reply_to(message, "تم الغاء الكتم ✅")
