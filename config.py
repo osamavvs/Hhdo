f5a12bce3bace6939c5e1b0774340cb8f0e5c57a
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-03 09:07:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 class Config:
-    OWNER_ID = 5406798224  # ايديك يا أسامة
-    OWNER_USERNAME = "U_K44"  # يوزرك
-    BOT_NAME = "بوت كرستال"
-    CH_USER = "BBABB9"  # قناة السورس
-    SUDO_USERS = [5406798224] # قائمة المطورين
+    # --- بيانات الربط الأساسية ---
+    API_ID = 24752047
+    API_HASH = "5b8a468627791bdd36a8c361913b0b72"
+    
+    # --- قاعدة البيانات ---
+    MONGO_DB_URI = "mongodb+srv://asopes111_db_user:urKzCqP4yrMpSUyq@cluster0.haza5xq.mongodb.net/?appName=Cluster0"
+    
+    # --- الهوية والإشعارات ---
+    OWNER_ID = 8074717568
+    LOG_GROUP_ID = -1005129765852  # أضفت لك -100 لأن التليجرام يحتاجها للكروبات
+    
+    # --- حقوق السورس ---
+    OWNER_USERNAME = "U_K44"
+    CH_USER = "BBABB9"
