227e2bdb0479edc3f27930a9c70589b80c90e438
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-04 17:24:23 +0300

    Update bot.py

--- core/bot.py
+++ core/bot.py
@@ -1,1 +1,11 @@
 
+from pyrogram import Client
+from config import API_ID, API_HASH, BOT_TOKEN
+
+app = Client(
+    "ArabMusicBot",
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="plugins")
+)
