4e2407c0161233ef1b2baa58b1a4c170b5bee32f
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-04 18:49:07 +0300

    Update play.py

--- plugins/play.py
+++ plugins/play.py
@@ -1,1 +1,10 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
 
+@Client.on_message(filters.command(["play", "تشغيل"]) & filters.group)
+async def play_command(client: Client, message: Message):
+    if len(message.command) < 2:
+        return await message.reply_text("يرجى كتابة اسم الأغنية بعد الأمر (مثال: تشغيل عراقي)")
+    
+    query = message.text.split(None, 1)[1]
+    await message.reply_text(f"🔎 جاري البحث عن: **{query}**...\nسيتم التشغيل قريباً!")
