58d7638bc0c2aa39c4cee9e376362bc440234ecc
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-04 17:26:12 +0300

    Update downloader.py

--- utils/downloader.py
+++ utils/downloader.py
@@ -1,1 +1,10 @@
+from yt_dlp import YoutubeDL
 
+def download_song(query):
+    ydl_opts = {
+        "format": "bestaudio",
+        "outtmpl": "downloads/%(title)s.%(ext)s",
+        "quiet": True
+    }
+
+    with YoutubeDL(ydl_opts) as ydl
