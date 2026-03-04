9ddf9076bf85a6b23994d06d89cb35f7f4bc184b
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-04 18:35:06 +0300

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,12 +1,5 @@
+from pyrogram import Client, filters
 
-from pyrogram import filters
-from pyrogram.types import Message
-from core.bot import app
-
-@app.on_message(filters.private & filters.text)
-async def start_handler(_, message: Message):
-    await message.reply(
-        "🎵 اهلا بك في بوت الميوزك\n\n"
-        "اكتب في الكروب:\n"
-        "تشغيل + اسم الاغنية"
-    )
+@Client.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+    await message.reply_text("💎 البوت شغال الآن!")
