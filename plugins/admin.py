e27d9afe51485c98b99763230e3a132f1a078cad
osamavvs <131695308+osamavvs@users.noreply.github.com>
2026-03-04 17:07:36 +0300

    Delete plugins directory

--- plugins/admin.py
+++
@@ -1,5 +0,0 @@
-from pyrogram import Client, filters
-
-@Client.on_message(filters.command("id") & filters.private)
-async def get_id(bot, message):
-    await message.reply_text(f"ايديك هو: <code>{message.from_user.id}</code>", parse_mode="html")
